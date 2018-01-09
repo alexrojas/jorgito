bd5b0a18ff68cb655b2c50c697b3b11d1a78685f
alexrojas <alex.rojas@agintechllc.com>
2018-01-09 14:10:40 -0500

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'index2' => 'users#index2'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
